a4075f1531765f6ad30cb89a71cf1170a0d0f90c
Aaron Vaz (Aidan at college) <80377344+aaaronvaz@users.noreply.github.com>
2021-03-17 10:28:49 +0530

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,15 @@
+function server_notice(){
+    alert("Server-down: view offline-!");
+}
+function cart_confirm(){
+    alert(" Added to cart.");
+}
+function review_submit(){
+    
+    alert("Feedback Submitted!\nTHANK YOU");
+}
+//server_notice();
+
+
+
+
